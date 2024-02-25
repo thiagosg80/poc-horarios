9ad57485399e746640b3d4ba5a1fbfaa2b24c316
Thiago Goncalves <thiagosg80@gmail.com>
2024-02-25 19:22:24 -0300

    Usa estrutura de turmas modificada de listas para mapa

--- function/set_organizacao.py
+++ function/set_organizacao.py
@@ -3,15 +3,14 @@ from typing import List
 from professor.model.disponibilidade import Disponibilidade
 from professor.model.professor import Professor
 from turma.model.grade.periodo import Periodo
-from turma.model.turma import Turma
 
 
-def set_organizacao(professor: Professor, periodos: List[Periodo], turmas: List[Turma]) -> None:
+def set_organizacao(professor: Professor, periodos: List[Periodo], turmas_map: dict) -> None:
     dias_disponiveis = list(map(lambda i: i.dia, professor.disponibilidades))
     periodos_filtrados = list(filter(lambda i: i.dia in dias_disponiveis and i.turma in professor.turmas, periodos))
     periodos_ja_alocados = []
     for periodo in periodos_filtrados:
-        turma = list(filter(lambda i: i.id == periodo.turma, turmas))[0]
+        turma = turmas_map[periodo.turma]
         disciplina = list(filter(lambda i: i.id == professor.disciplina, turma.disciplinas))[0]
 
         disponibilidade = __get_disponibilidade__(professor, turma.turno, periodo.dia) \
@@ -20,8 +19,9 @@ def set_organizacao(professor: Professor, periodos: List[Periodo], turmas: List[
         periodo_descricao = professor.disciplina + ' - ' + professor.nome
 
         if (disponibilidade.quantidade_de_periodos > 0 and __nao_tem_marcacoes_consecutivas_ainda__(
-                3, periodos_filtrados, turma.id, periodo.dia, periodo_descricao) and disciplina.quantidade_periodos > 0
-                and not periodo.descricao and __nao_ha_periodos_simultaneos__(periodos_ja_alocados, periodo, turmas)):
+                3, periodos_filtrados, periodo.turma, periodo.dia, periodo_descricao) and
+                disciplina.quantidade_periodos > 0 and not periodo.descricao and __nao_ha_periodos_simultaneos__(
+                    periodos_ja_alocados, periodo, turmas_map)):
 
             periodo.descricao = periodo_descricao
             disponibilidade.quantidade_de_periodos -= 1
@@ -50,18 +50,13 @@ def __nao_tem_marcacoes_consecutivas_ainda__(quantidade: int, periodos: List[Per
     return len(periodos_filtrados) < quantidade
 
 
-def __nao_ha_periodos_simultaneos__(periodos_ja_alocados: List[Periodo], periodo: Periodo,
-                                    turmas: List[Turma]) -> bool:
-
-    simultaneos = list(filter(lambda i: __is_simultaneos_callback__(i, periodo, turmas), periodos_ja_alocados))
+def __nao_ha_periodos_simultaneos__(periodos_ja_alocados: List[Periodo], periodo: Periodo, turmas_map: dict) -> bool:
+    simultaneos = list(filter(lambda i: __is_simultaneos_callback__(i, periodo, turmas_map), periodos_ja_alocados))
 
     return len(simultaneos) == 0
 
 
-def __is_simultaneos_callback__(i, periodo, turmas) -> bool:
-    return i.dia == periodo.dia and i.ordem == periodo.ordem and __get_turno__(i.turma, turmas) == __get_turno__(
-        periodo.turma, turmas)
+def __is_simultaneos_callback__(i: Periodo, periodo: Periodo, turmas_map: dict) -> bool:
+    is_same_turno = turmas_map[i.turma].turno == turmas_map[periodo.turma].turno
 
-
-def __get_turno__(turma_id: str, turmas: List[Turma]) -> str:
-    return list(filter(lambda i: i.id == turma_id, turmas))[0].turno
+    return i.dia == periodo.dia and i.ordem == periodo.ordem and is_same_turno
